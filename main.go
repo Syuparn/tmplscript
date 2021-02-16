a83d48009bb1589c55f745b1886001fcfa4b9600
Syuparn <s.hello.spagetti@gmail.com>
2021-02-16 09:23:31 +0900

    add repl

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"text/template"
@@ -9,17 +11,31 @@ import (
 	"github.com/Masterminds/sprig"
 )
 
+var (
+	runsREPL = flag.Bool("i", false, "run interactive REPL instead")
+)
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "template must be passed to argument")
+	flag.Parse()
+
+	if *runsREPL {
+		runREPLMode()
 		return
 	}
-	tmplStr := os.Args[1]
 
+	if flag.Arg(0) == "" {
+		fmt.Fprintf(os.Stderr, "template must be passed to argument\n")
+		return
+	}
+
+	runPipeMode(flag.Arg(0))
+}
+
+func runPipeMode(tmplStr string) {
 	// NOTE: FuncMap is for html/template, TxtFuncMap is for text/template
 	tmpl, err := template.New("tmpl").Funcs(sprig.TxtFuncMap()).Parse(tmplStr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "syntax error:\n%v", err)
+		fmt.Fprintf(os.Stderr, "template error:\n%v\n", err)
 		return
 	}
 
@@ -29,8 +45,59 @@ func main() {
 
 		err = tmpl.Execute(os.Stdout, line)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "runtime error:\n%v", err)
+			fmt.Fprintf(os.Stderr, "runtime error:\n%v\n", err)
 			return
 		}
+	}
+}
+
+func runREPLMode() {
+	tmplGen := template.New("tmpl").Funcs(sprig.TxtFuncMap())
+	scanner := bufio.NewScanner(os.Stdin)
+	tmplStr := ""
+	lineNum := 1
+
+	for {
+		// show prompt
+		fmt.Printf("tmpl:%d> ", lineNum)
+
+		ok := scanner.Scan()
+		if !ok {
+			return
+		}
+
+		line := scanner.Text() + "\n"
+
+		// NOTE: whole history is necessary to refer previous variable statement
+		tmpl, err := tmplGen.Parse(tmplStr + line)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "template error:\n%v\n", err)
+			continue
+		}
+
+		out := new(bytes.Buffer)
+		err = tmpl.Execute(out, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "runtime error:\n%v\n", err)
+			continue
+		}
+		// print only last line, which corresponds to the latest input
+		// NOTE: break line is unneccessary because it already exists
+		fmt.Print(lastLine(out))
+
+		tmplStr = tmplStr + line
+		lineNum++
+	}
+}
+
+func lastLine(out *bytes.Buffer) string {
+	last := ""
+
+	for {
+		line, err := out.ReadString('\n')
+		if err != nil {
+			return last
+		}
+		last = line
 	}
 }
